4358d7cfa99e7e7ca5b3aa1a33c46e63d0d4dd17
Shrikant <shrikanttomar11@gmail.com>
2017-05-12 13:00:46 +0530

    Dev 1

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,47 +3,55 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-        <section className="login-page-4 parallex">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12 col-sm-12 col-xs-12">
-                        <div className="login-container">
-                            <div className="loginbox">
-                                <img src="images/logo.png" alt="logo" className="img-responsive center-block" />
-                                <div className="loginbox-title">sign in using social accounts</div>
-                                <ul className="social-network social-circle onwhite">
-                                    <li><a href="#" className="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
-                                    <li><a href="#" className="icoTwitter" title="Twitter"><i className="fa fa-twitter"></i></a></li>
-                                    <li><a href="#" className="icoGoogle" title="Google +"><i className="fa fa-google-plus"></i></a></li>
-                                    <li><a href="#" className="icoLinkedin" title="Linkedin +"><i className="fa fa-linkedin"></i></a></li>
-                                </ul>
-                                <div className="loginbox-or">
-                                    <div className="or-line"></div>
-                                    <div className="or">OR</div>
-                                </div>
-                                <div className="form-group">
-                                    <label>Email: <span className="required">*</span></label>
-                                    <input placeholder="" className="form-control" type="email" />
-                                </div>
-                                <div className="form-group">
-                                    <label>Password: <span className="required">*</span></label>
-                                    <input placeholder="" className="form-control" type="password" />
-                                </div>
-                                <div className="loginbox-forgot">
-                                    <a href="">Forgot Password?</a>
-                                </div>
-                                <div className="loginbox-submit">
-                                    <a href="/employee-dashboard"><input type="button" className="btn btn-default btn-block" value="Login" /></a>
-                                </div>
-                                <div className="loginbox-signup">
-                                    <a href="/register">Sign Up With Email</a>
+        <div className="page category-page">
+            <div id="spinner">
+                <div className="spinner-img">
+                    <img alt="Opportunities Preloader" src="images/loader.gif" />
+                    <h2>Please Wait.....</h2>
+                </div>
+            </div>
+            <section className="login-page-4 parallex">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 col-sm-12 col-xs-12">
+                            <div className="login-container">
+                                <div className="loginbox">
+                                    <img src="images/logo.png" alt="logo" className="img-responsive center-block" />
+                                    <div className="loginbox-title">sign in using social accounts</div>
+                                    <ul className="social-network social-circle onwhite">
+                                        <li><a href="#" className="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
+                                        <li><a href="#" className="icoTwitter" title="Twitter"><i className="fa fa-twitter"></i></a></li>
+                                        <li><a href="#" className="icoGoogle" title="Google +"><i className="fa fa-google-plus"></i></a></li>
+                                        <li><a href="#" className="icoLinkedin" title="Linkedin +"><i className="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                    <div className="loginbox-or">
+                                        <div className="or-line"></div>
+                                        <div className="or">OR</div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Email: <span className="required">*</span></label>
+                                        <input placeholder="" className="form-control" type="email" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Password: <span className="required">*</span></label>
+                                        <input placeholder="" className="form-control" type="password" />
+                                    </div>
+                                    <div className="loginbox-forgot">
+                                        <a href="">Forgot Password?</a>
+                                    </div>
+                                    <div className="loginbox-submit">
+                                        <a href="/employee-dashboard"><input type="button" className="btn btn-default btn-block" value="Login" /></a>
+                                    </div>
+                                    <div className="loginbox-signup">
+                                        <a href="/register">Sign Up With Email</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     );
   }
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,52 +3,60 @@ import React, { Component } from 'react';
 class Register extends Component {
   	render() {
     	return (
-    		<section className="login-page-4 parallex">
-	            <div className="container">
-	                <div className="row">
-	                    <div className="col-md-12 col-sm-12 col-xs-12">
-	                        <div className="login-container">
-	                            <div className="loginbox">
-	                                <div className="loginbox-title">Sign Up using social accounts</div>
-	                                <ul className="social-network social-circle onwhite">
-	                                    <li><a href="#" className="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
-	                                    <li><a href="#" className="icoTwitter" title="Twitter"><i className="fa fa-twitter"></i></a></li>
-	                                    <li><a href="#" className="icoGoogle" title="Google +"><i className="fa fa-google-plus"></i></a></li>
-	                                    <li><a href="#" className="icoLinkedin" title="Linkedin +"><i className="fa fa-linkedin"></i></a></li>
-	                                </ul>
-	                                <div className="loginbox-or">
-	                                    <div className="or-line"></div>
-	                                    <div className="or">OR</div>
-	                                </div>
-	                                <div className="form-group">
-	                                    <label>Username: <span className="required">*</span></label>
-	                                    <input placeholder="" className="form-control" type="text" />
-	                                </div>
-	                                <div className="form-group">
-	                                    <label>Email: <span className="required">*</span></label>
-	                                    <input placeholder="" className="form-control" type="email" />
-	                                </div>
-	                                <div className="form-group">
-	                                    <label>Password: <span className="required">*</span></label>
-	                                    <input placeholder="" className="form-control" type="password" />
-	                                </div>
-	                                <div className="form-group">
-	                                    <label>Confirm Password: <span className="required">*</span></label>
-	                                    <input placeholder="" className="form-control" type="password" />
-	                                </div>
-	                                <div className="loginbox-forgot">
-	                                    <input type="checkbox" /> I accept <a href="">Term and consitions?</a>
-	                                </div>
-	                                <div className="loginbox-submit">
-	                                    <input type="button" className="btn btn-default btn-block" value="Register" />
-	                                </div>
-	                                <div className="loginbox-signup"> Already have account <a href="/login">Sign in</a> </div>
-	                            </div>
-	                        </div>
-	                    </div>
+    		<div className="page category-page">
+	            <div id="spinner">
+	                <div className="spinner-img">
+	                    <img alt="Opportunities Preloader" src="images/loader.gif" />
+	                    <h2>Please Wait.....</h2>
 	                </div>
 	            </div>
-	        </section>
+	    		<section className="login-page-4 parallex">
+		            <div className="container">
+		                <div className="row">
+		                    <div className="col-md-12 col-sm-12 col-xs-12">
+		                        <div className="login-container">
+		                            <div className="loginbox">
+		                                <div className="loginbox-title">Sign Up using social accounts</div>
+		                                <ul className="social-network social-circle onwhite">
+		                                    <li><a href="#" className="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
+		                                    <li><a href="#" className="icoTwitter" title="Twitter"><i className="fa fa-twitter"></i></a></li>
+		                                    <li><a href="#" className="icoGoogle" title="Google +"><i className="fa fa-google-plus"></i></a></li>
+		                                    <li><a href="#" className="icoLinkedin" title="Linkedin +"><i className="fa fa-linkedin"></i></a></li>
+		                                </ul>
+		                                <div className="loginbox-or">
+		                                    <div className="or-line"></div>
+		                                    <div className="or">OR</div>
+		                                </div>
+		                                <div className="form-group">
+		                                    <label>Username: <span className="required">*</span></label>
+		                                    <input placeholder="" className="form-control" type="text" />
+		                                </div>
+		                                <div className="form-group">
+		                                    <label>Email: <span className="required">*</span></label>
+		                                    <input placeholder="" className="form-control" type="email" />
+		                                </div>
+		                                <div className="form-group">
+		                                    <label>Password: <span className="required">*</span></label>
+		                                    <input placeholder="" className="form-control" type="password" />
+		                                </div>
+		                                <div className="form-group">
+		                                    <label>Confirm Password: <span className="required">*</span></label>
+		                                    <input placeholder="" className="form-control" type="password" />
+		                                </div>
+		                                <div className="loginbox-forgot">
+		                                    <input type="checkbox" /> I accept <a href="">Term and consitions?</a>
+		                                </div>
+		                                <div className="loginbox-submit">
+		                                    <input type="button" className="btn btn-default btn-block" value="Register" />
+		                                </div>
+		                                <div className="loginbox-signup"> Already have account <a href="/login">Sign in</a> </div>
+		                            </div>
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </section>
+		    </div>
     	);
 	}
 
